45401247f8e4acb8185d63e6ca079d073aec29ce
Anvo00 <vitoriccio03@gmail.com>
2025-07-25 17:14:03 +0200

    [Add] modules, controllers and services

--- event-backend/src/app.module.ts
+++ event-backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, TicketsModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
