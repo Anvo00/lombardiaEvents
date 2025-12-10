bb867943e4f39d08c6e015703f847f3d97e08a07
AlessAnto <a.aless4ndra@gmail.com>
2025-12-10 22:26:42 +0100

    [Fix] Ui Changes

--- eventFrontend/src/app/auth/auth.ts
+++ eventFrontend/src/app/auth/auth.ts
@@ -4,11 +4,13 @@ import { AuthService } from './auth.service';
 import { Role } from '@shared/role.enum';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { Navbar } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Navbar, Footer],
   templateUrl: './auth.html',
   styleUrls: ['./auth.scss']
 })
